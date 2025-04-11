1ba6350a13aa3060bc68ddb141b778cfa9c72cca
Vinson 2 <vinsonhe2@gmail.com>
2025-04-10 19:36:26 -0700

    Populate current overview of location

--- src/dom.js
+++ src/dom.js
@@ -4,6 +4,7 @@ import { format, parseISO } from 'date-fns';
 export function showInitialLocation() {
     populateDateHeading();
     setWeatherIcon(locationData.currentDay.icon);
+    populateOverview();
 }
 
 function populateDateHeading() {
@@ -19,4 +20,16 @@ async function setWeatherIcon(icon) {
     const weatherObject = document.querySelector('.weather-icon');
     const svgUrl = await import(`./assets/${icon}.svg`);
     weatherObject.data = svgUrl.default;
+}
+
+function populateOverview() {
+    const city = document.querySelector('.city');
+    const country = document.querySelector('.country');
+    const currentTemperature = document.querySelector('.current-temperature');
+
+    const splitLocation = locationData.currentDay.resolvedAddress.split(', ');
+    
+    city.textContent = splitLocation[0];
+    country.textContent = splitLocation[splitLocation.length - 1];
+    currentTemperature.textContent = `${locationData.currentDay.temp}° ${locationData.currentDay.conditions}`;
 }
